9e7aed6ac5949e02f311c65bbb22194e35ecbf9b
Satinder Singh <agnosticode@gmail.com>
2018-03-20 21:52:49 +0530

    Task List [Part-2]-Delete and Filter Tasks

--- app.js
+++ app.js
@@ -12,6 +12,12 @@ LoadEventListeners();
 function LoadEventListeners(){
     //Add task event
     form.addEventListener('submit',addTask);
+    //Remove task event
+    taskList.addEventListener('click',removeTask);
+    //Clear task event
+    clearBtn.addEventListener('click',clearTasks);
+    //Filter task event
+    filter.addEventListener('keyup',filterTasks);
 };
 
 
@@ -40,4 +46,35 @@ function addTask(e){
     taskList.appendChild(li);
     //Clear input
     taskInput.value='';
-}
+};
+
+//Remove Task
+function removeTask(e){
+    if(e.target.parentElement.classList.contains('delete-item'))
+    if(confirm("Are You Sure?")){
+        e.target.parentElement.parentElement.remove();
+    }
+};
+
+//Clear Tasks
+function clearTasks(){
+    //taskList.innerHTML='';
+    //Faster
+    while(taskList.firstChild){
+        taskList.removeChild(taskList.firstChild);
+    };
+    //https://jsperf.com/innerhtml-vs-removechild
+};
+
+//Filter tasks***************IMPORTANT
+function filterTasks(e){
+    const text=e.target.value.toLowerCase();
+    document.querySelectorAll('.collection-item').forEach(function(task){
+        const item=task.textContent;
+        if(item.toLowerCase().indexOf(text) != -1){
+            task.style.display="block";
+        }else{
+            task.style.display="none";
+        };
+    });
+};
